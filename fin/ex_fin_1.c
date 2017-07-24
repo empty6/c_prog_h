c15f4fa91887223f7e28278053ab9ac46192acc5
empty6 <empty.bnt@gmail.com>
2017-07-24 14:22:22 +0900

    f-1 end?

--- fin/ex_fin_1.c
+++ fin/ex_fin_1.c
@@ -6,54 +6,104 @@
 #include <stdlib.h>
 #include <string.h>
 
+#define ROW_LEN 21
+
+int count = 0;
+
 typedef struct tnode{
   char *term;
-  int dfreq;
   struct tnode *left;
   struct tnode *right;
-  lnode *pos;
 } tnode;
 
+void createTNode(tnode**, char*, int);
 void insertTerm(tnode**, char*);
-void createTNode(tnode**, vhar*);
-void printTree(tnode**);
-void freeTree(tnode**);
+void printTree(tnode*);
+void freeTree(tnode*);
 
-int main(void){
-  tnode *root;
+int main(int argc, char **argv){
+  FILE *fp;
+  char *fname, termTmp[ROW_LEN];
+  tnode *root = NULL;
 
+  //get input file
+  if(argc!=2){
+    fprintf(stderr, "Usage: %s prob_file\n", argv[0]);
+    return 1;
+  }
+  fname = argv[1];
+  fp = fopen(fname, "r");
+  if(fp==NULL){
+    fprintf(stderr,"input file error");
+    return 1;
+  }
 
+  while(fgets(termTmp, ROW_LEN, fp) != NULL){
+    insertTerm(&root, termTmp);
+  }
+  fclose(fp);
+
+  printTree(root);
+  freeTree(root);
 
   return 0;
 }
 
-//add
-void add_list(lnode **first, int id){
-printf("add -> %d\n", id);
-  lnode *new = malloc(sizeof(lnode));
+//add node isLeft=true:left
+void createTNode(tnode **parent, char *term, int isLeft){
+  tnode *new = malloc(sizeof(tnode));
   if(new == NULL) exit(2);  //malloc error
-  new->docid = id;
-  new->next = *first;
-  *first = new;
+  char *newTerm = malloc(sizeof(char) * (strlen(term) + 1));
+  if(newTerm == NULL) exit(2);  //malloc error
+  strcpy(newTerm, term);
+  new->term = newTerm;
+  new->left = NULL;
+  new->right = NULL;
+
+  if((*parent) != NULL){
+    if(isLeft){
+      (*parent)->left = new;
+    }else{
+      (*parent)->right = new;
+    }
+  }else{
+    *parent = new;
+  }
 }
 
-//insert id list
-void insert_list(lnode **first, int id){
-  lnode *pivot = *first;
-  if(pivot == NULL || pivot->docid > id){
-    add_list(first, id);
+//insert term
+void insertTerm(tnode **parent, char *term){
+  if((*parent) == NULL){
+    createTNode(parent, term, 1);
+  }else if(strcmp((*parent)->term, term) < 0){
+    //right
+    if((*parent)->right == NULL){
+      createTNode(parent, term, 0);
+    }else{
+      insertTerm(&((*parent)->right), term);
+    }
   }else{
-    //search
-    while(pivot != NULL){
-      if(pivot->next == NULL || (pivot->next)->docid > id){
-        add_list(&(pivot->next), id);
-        break;
-      }
-      pivot = pivot->next;
+    //left
+    if((*parent)->left == NULL){
+      createTNode(parent, term, 1);
+    }else{
+      insertTerm(&((*parent)->left), term);
     }
   }
 }
 
-void add_tree(tnode **prev, char *term){
+void printTree(tnode *parent){
+  if(parent != NULL){
+    printTree(parent->left);
+    printf("%s", parent->term);
+    printTree(parent->right);
+  }
+}
 
+void freeTree(tnode *parent){
+  if(parent != NULL){
+    freeTree(parent->left);
+    freeTree(parent->right);
+    free(parent);
+  }
 }
